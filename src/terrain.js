b3c8b532c1cb95d07d0b755316e89f04bcb5b457
groverburger <zachary.m.booth@gmail.com>
2022-10-03 02:39:59 -0700

    fixed textures

--- src/terrain.js
+++ src/terrain.js
@@ -301,7 +301,7 @@ export default class Terrain extends Thing {
       }
       else if (getStage() == 3) {
         if (tileType == 1) {return "techstone"}
-        if (tileType == 2) {return "finale"}
+        if (tileType == 2) {return "tech1"}
         if (tileType == 3) {return "techstone"}
         if (tileType == 4) {return "goldenWall"}
         return "techstone"
